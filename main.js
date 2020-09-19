9559e06322587d076c78f463574cb3da008ba124
Josua Sinaga <josua123690707@gmail.com>
2020-09-19 08:48:10 +0700

    updating color and background

--- main.js
+++ main.js
@@ -1,26 +1,30 @@
 let circle = document.getElementsByClassName("circle");
 let circleShadow = document.getElementsByClassName("circle-shadow");
+let body = document.querySelector("body");
 
 let colors = [
-  "#cff6cf",
-  "#ffe0ac",
-  "#ffacb7",
-  "#beebe9",
-  "#a0ffe6",
-  "#be9fe1",
-  "#e1ccec",
-  "#f6ecbf",
-  "#f6a7ba",
-  "#b0deff",
-  "#c8e7ed",
-  "#ff8c94",
-  "#cadefc",
+  "#ef5350",
+  "#ec407a",
+  "#ec407a",
+  "#7e57c2",
+  "#5c6bc0",
+  "#42a5f5",
+  "#29b6f6",
+  "#d4e157",
+  "#ffee58",
+  "#ffca28",
+  "#ffa726",
+  "#ff7043"
 ];
 
 let onCircleClick = () => {
   let color = colors[Math.floor(Math.random() * colors.length - 1)];
   circle[0].style.backgroundColor = color;
   console.log(color);
+  
+  let bgColor = color + "22";
+  body.style.backgroundColor  = bgColor;
+  console.log(`bgColor : ${bgColor}`);
 };
 
 circle[0].addEventListener("click", onCircleClick);
